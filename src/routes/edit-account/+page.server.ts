06fccac84f240ecccd6915af43d6fbfcb250ef9a
draco696 <dagixmeko@gmail.com>
2024-01-02 23:53:16 +0300

    edit account page

--- src/routes/edit-account/+page.server.ts
+++ src/routes/edit-account/+page.server.ts
@@ -1,5 +1,10 @@
+import { S3_BUCKET_NAME } from '$env/static/private';
+import { getFile, s3 } from '$lib/utils/aws-file.js';
 import { prisma } from '$lib/utils/prisma.js';
+import { fail } from '@sveltejs/kit';
 import { superValidate } from 'sveltekit-superforms/server';
+import bcrypt from 'bcrypt';
+
 import z from 'zod';
 
 const editUserSchema = z.object({
@@ -13,26 +18,85 @@ const editUserSchema = z.object({
 });
 export const load = async (event) => {
 	const session = (await event.locals.getSession()) as EnhancedSessionType | null;
-	const employee = await prisma.user.findFirst({
-		where: {
-			id: session?.authUser.id
-		},
-		include: {
-			Employee: true
+	let fileUrl;
+	if (session?.authUser.Employee.image) {
+		fileUrl = await getFile(session?.authUser.Employee.image);
+	}
+
+	return { fileUrl };
+};
+
+export const actions = {
+	profilePicture: async (event) => {
+		const data = await event.request.formData();
+		const file = data.getAll('profilePicture');
+		const session = (await event.locals.getSession()) as EnhancedSessionType | null;
+
+		const allNewFiles = file.map(async (file) => {
+			console.log({ file });
+			if (!(file instanceof File)) {
+				return fail(500, { errorMessage: 'Issue with the file uploaded.' });
+			}
+			console.log({ file });
+
+			const buffer = await file.arrayBuffer();
+			const send = Buffer.from(buffer);
+			try {
+				await s3
+					.putObject({
+						Bucket: S3_BUCKET_NAME,
+						Key: `profilePicture/${session?.authUser.Employee.id}`,
+						Body: send
+					})
+					.promise();
+
+				const newFile = await prisma.employee.update({
+					where: {
+						id: session?.authUser.Employee.id
+					},
+					data: {
+						image: `profilePicture/${session?.authUser.Employee.id}`
+					}
+				});
+
+				console.log({ newFile });
+			} catch (error) {
+				console.log(error as Error);
+			}
+		});
+	},
+	editPassword: async (event) => {
+		const data = await event.request.formData();
+		const oldPassword = data.get('oldPassword');
+		const newPassword = data.get('newPassword');
+
+		const session = (await event.locals.getSession()) as EnhancedSessionType | null;
+		const user = await prisma.user.findFirst({
+			where: {
+				id: session?.authUser.id
+			}
+		});
+		if (!oldPassword || typeof oldPassword !== 'string') {
+			return fail(500, { errorMessage: 'New password not found.' });
 		}
-	});
+		if (!newPassword || typeof newPassword !== 'string') {
+			return fail(500, { errorMessage: 'New password not found.' });
+		}
 
-	const editUserForm = await superValidate(
-		{
-			userName: employee?.userName || '',
-			phoneNumber: employee?.phoneNumber || '',
-			email: employee?.email,
-			address: employee?.Employee?.address,
-			bloodType: employee?.Employee?.bloodType,
-			height: employee?.Employee?.height
-		},
-		editUserSchema
-	);
+		const authorized = await bcrypt.compare(oldPassword, user?.jwtPassword || '');
+		if (!authorized) {
+			return fail(500, { errorMessage: 'Wrong Password' });
+		}
 
-	return { editUserForm };
+		const newPasswordHash = await bcrypt.hash(newPassword, 10);
+		const passwordChanged = await prisma.user.update({
+			where: {
+				id: session?.authUser.id
+			},
+			data: {
+				jwtPassword: newPasswordHash
+			}
+		});
+		return { passwordChanged };
+	}
 };
--- src/routes/edit-account/+page.svelte
+++ src/routes/edit-account/+page.svelte
@@ -1,70 +1,222 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import { page } from '$app/stores';
-	$: console.log($page.data.session?.authUser);
+	import FileUp from '$lib/assets/file-up.svg.svelte';
+	import FileUpload from '$lib/assets/file-upload.svg.svelte';
+	import { toast } from '@zerodevx/svelte-toast';
+	export let data;
+	export let form;
+
+	let editable = false;
+	let frontFileData: string;
+
+	$: form?.errorMessage ? toast.push(form.errorMessage) : null;
+	$: form?.passwordChanged ? toast.push('Password changed') : null;
 </script>
 
-<form class="mx-10 my-12 items-start bg-white rounded-xl p-8">
-	<div class="text-2xl text-primary mb-6">Account Setting</div>
-	<div class="grid grid-cols-3 gap-4">
-		<label class="grid gap-2">
-			<span class="text-primary font-medium"> Username </span>
-			<input
-				class="w-96 border-[1px] border-primary/60 rounded-md p-2"
-				name="userName"
-				value={$page.data.session?.authUser.userName}
-			/>
-		</label>
-		<label class="grid gap-2">
-			<span class="text-primary font-medium"> Phone Number </span>
-			<input
-				class="w-96 border-[1px] border-primary/60 rounded-md p-2"
-				name="phoneNumber"
-				value={$page.data.session?.authUser.phoneNumber}
-			/>
-		</label>
-		<label class="grid gap-2">
-			<span class="text-primary font-medium"> Email </span>
-			<input
-				disabled
-				class="w-96 border-[1px] border-primary/60 rounded-md p-2"
-				name="email"
-				value={$page.data.session?.authUser.email}
-			/>
-		</label>
+<div class="mx-10 my-12 grid justify-items-stretch bg-white rounded-xl p-8">
+	<div class="justify-self-center text-3xl text-primary font-medium mb-8">Account Setting</div>
+	{#if data.fileUrl}
+		<form
+			method="post"
+			action="?/profilePicture"
+			class="justify-self-center"
+			enctype="multipart/form-data"
+		>
+			<label>
+				<input
+					class="hidden"
+					type="file"
+					name="profilePicture"
+					accept="image/*"
+					on:change={(e) => {
+						const data = e.currentTarget.files;
+						if (data) {
+							console.log({ data });
+							frontFileData = data[0].name;
+						}
+						e.currentTarget.form?.submit();
+					}}
+				/>
+				<img src={data.fileUrl} alt="profile" class="max-w-[120px] rounded-xl" />
+			</label>
+		</form>
+	{:else}
+		<form
+			method="post"
+			action="?/profilePicture"
+			class="justify-self-center"
+			enctype="multipart/form-data"
+		>
+			<label>
+				<input
+					class="hidden"
+					type="file"
+					name="profilePicture"
+					accept="image/*"
+					on:change={(e) => {
+						const data = e.currentTarget.files;
+						if (data) {
+							console.log({ data });
+							frontFileData = data[0].name;
+						}
+						e.currentTarget.form?.submit();
+					}}
+				/>
+				<div class=" ">
+					{#if frontFileData}
+						<div
+							class="relative border-[1px] max-w-[180px] border-primary border-dashed rounded-lg gap-2 items-center justify-center"
+						>
+							<div class=" relative z-10 w-32 h-36" />
 
-		<label class="grid gap-2">
-			<span class="text-primary font-medium"> Address </span>
-			<input
-				class="w-96 border-[1px] border-primary/60 rounded-md p-2"
-				name="address"
-				value={$page.data.session?.authUser.Employee.address}
-			/>
-		</label>
-		<label class="grid gap-2">
-			<span class="text-primary font-medium"> Date of Birth </span>
-			<input
-				type="date"
-				class="w-96 border-[1px] border-primary/60 rounded-md p-2"
-				name="dateOfBirth"
-				value={$page.data.session?.authUser.Employee.dateOfBirth}
-			/>
-		</label>
-		<label class="grid gap-2">
-			<span class="text-primary font-medium"> Blood Type </span>
-			<input
-				class="w-96 border-[1px] border-primary/60 rounded-md p-2"
-				name="bloodType"
-				value={$page.data.session?.authUser.Employee.bloodType}
-			/>
-		</label>
-		<label class="grid gap-2">
-			<span class="text-primary font-medium"> Height </span>
-			<input
-				class="w-96 border-[1px] border-primary/60 rounded-md p-2"
-				name="height"
-				value={$page.data.session?.authUser.Employee.height}
-			/>
-		</label>
-	</div>
-	<button class="bg-primary text-white w-96 rounded-md py-2 mt-6" type="submit">Edit</button>
-</form>
+							<div class="absolute top-0 w-full h-full left-0 z-30">
+								<div class="flex flex-col gap-2 justify-center items-center h-full">
+									<FileUpload class="h-6 w-6 flex-shrink-0 ml-2 text-black" />
+									<p class="text-sm ml-2 py-2">{frontFileData}</p>
+								</div>
+							</div>
+						</div>
+					{:else}
+						<div
+							class="relative border-[1px] border-primary border-dashed rounded-lg flex-1 flex-shrink-0 max-w-[180px] max-h-96 gap-2 items-center justify-center"
+						>
+							<div class=" relative z-10 w-44 h-36" />
+							<div class="absolute top-0 w-full h-full left-0 z-30">
+								<div class="flex flex-col gap-2 justify-center items-center h-full">
+									<FileUp class="text-primary w-7 h-7" />
+									<span class="text-xs">Upload Profile Picture</span>
+								</div>
+							</div>
+						</div>
+					{/if}
+				</div>
+			</label>
+		</form>
+	{/if}
+
+	<form method="post" action="?/editAccount" class="mt-12 grid justify-items-stretch">
+		<div class="grid grid-cols-3 gap-4">
+			<label class="grid gap-2">
+				<span class="text-primary font-medium"> Username </span>
+				<input
+					class="w-96 border-[1px] border-primary/60 rounded-md p-2"
+					name="userName"
+					value={$page.data.session?.authUser.userName}
+					on:change={() => (editable = true)}
+				/>
+			</label>
+			<label class="grid gap-2">
+				<span class="text-primary font-medium"> Phone Number </span>
+				<input
+					class="w-96 border-[1px] border-primary/60 rounded-md p-2"
+					name="phoneNumber"
+					value={$page.data.session?.authUser.phoneNumber}
+					on:change={() => (editable = true)}
+				/>
+			</label>
+			<label class="grid gap-2">
+				<span class="text-primary font-medium"> Email </span>
+				<input
+					disabled
+					class="w-96 border-[1px] border-primary/60 rounded-md p-2"
+					name="email"
+					value={$page.data.session?.authUser.email}
+				/>
+			</label>
+
+			<label class="grid gap-2">
+				<span class="text-primary font-medium"> Address </span>
+				<input
+					class="w-96 border-[1px] border-primary/60 rounded-md p-2"
+					name="address"
+					value={$page.data.session?.authUser.Employee.address}
+					on:change={() => (editable = true)}
+				/>
+			</label>
+			<label class="grid gap-2">
+				<span class="text-primary font-medium"> Date of Birth </span>
+				<input
+					type="date"
+					class="w-96 border-[1px] border-primary/60 rounded-md p-2"
+					name="dateOfBirth"
+					value={$page.data.session?.authUser.Employee.dateOfBirth}
+					on:change={() => (editable = true)}
+				/>
+			</label>
+			<label class="grid gap-2">
+				<span class="text-primary font-medium"> Blood Type </span>
+				<input
+					class="w-96 border-[1px] border-primary/60 rounded-md p-2"
+					name="bloodType"
+					value={$page.data.session?.authUser.Employee.bloodType}
+					on:change={() => (editable = true)}
+				/>
+			</label>
+			<label class="grid gap-2">
+				<span class="text-primary font-medium"> Height </span>
+				<input
+					class="w-96 border-[1px] border-primary/60 rounded-md p-2"
+					name="height"
+					value={$page.data.session?.authUser.Employee.height}
+					on:change={() => (editable = true)}
+				/>
+			</label>
+		</div>
+		<button
+			class="bg-primary text-white w-96 justify-self-center rounded-md py-2 mt-6"
+			type="submit">Edit</button
+		>
+	</form>
+	<form
+		method="post"
+		action="?/editPassword"
+		class="mt-12 border-[1px] border-danger border-dashed p-4 rounded-2xl grid justify-items-stretch"
+		use:enhance={({ formData, cancel }) => {
+			const newPassword = formData.get('newPassword');
+			const confirmNewPassword = formData.get('confirmPassword');
+			if (newPassword !== confirmNewPassword) {
+				cancel();
+				toast.push('New password and confirm password must be the same');
+			}
+		}}
+	>
+		<p class="justify-self-center text-2xl text-primary font-medium mb-4">Change Password</p>
+		<div class="grid grid-cols-3 gap-4">
+			<label class="grid gap-2">
+				<input
+					class="w-96 border-[1px] border-primary/60 rounded-md p-2"
+					name="oldPassword"
+					placeholder="Old Password"
+					on:change={() => (editable = true)}
+					minlength="8"
+					type="password"
+				/>
+			</label>
+			<label class="grid gap-2">
+				<input
+					class="w-96 border-[1px] border-primary/60 rounded-md p-2"
+					name="newPassword"
+					placeholder="New Password"
+					on:change={() => (editable = true)}
+					minlength="8"
+					type="password"
+				/>
+			</label>
+			<label class="grid gap-2">
+				<input
+					class="w-96 border-[1px] border-primary/60 rounded-md p-2"
+					name="confirmPassword"
+					placeholder="Confirm Password"
+					on:change={() => (editable = true)}
+					minlength="8"
+					type="password"
+				/>
+			</label>
+		</div>
+		<button
+			class="bg-primary text-white w-96 justify-self-center rounded-md py-2 mt-6"
+			type="submit">Change Password</button
+		>
+	</form>
+</div>
