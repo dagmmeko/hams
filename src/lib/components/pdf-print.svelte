d3b80c61a7f49c1185eb4b1368e531b004da01fb
draco696 <dagixmeko@gmail.com>
2024-01-31 08:55:01 +0300

    header and footer for print

--- src/lib/components/pdf-print.svelte
+++ src/lib/components/pdf-print.svelte
@@ -2,6 +2,8 @@
 	import Printer from '$lib/assets/printer.svg.svelte';
 
 	import PrintPdf from 'svelte-printpdf';
+	import CPHeader from '$lib/assets/cp_header.png';
+	import CPFooter from '$lib/assets/cp_footer.png';
 
 	let print = false;
 	let className = '';
@@ -21,6 +23,8 @@
 		<Printer class="h-4 w-4" /> Print
 	</button>
 	<PrintPdf bind:print>
+		<img src={CPHeader} class="hidden print:block mb-6" alt="cp header" />
 		<slot />
+		<img src={CPFooter} class="hidden print:block fixed bottom-0 w-full" alt="cp header" />
 	</PrintPdf>
 </div>
